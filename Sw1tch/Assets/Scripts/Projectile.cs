81f39014e81d7b74709054bceec30bfb23f050be
FairyTamerRyles <54481765+FairyTamerRyles@users.noreply.github.com>
2022-05-18 10:23:34 -0500

    TilemapSetup

--- Sw1tch/Assets/Scripts/Projectile.cs
+++ Sw1tch/Assets/Scripts/Projectile.cs
@@ -37,8 +37,8 @@ public class Projectile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerVelocity = GameObject.Find(playerName).GetComponent<Rigidbody2D>().velocity;
-        rb.AddForce(playerVelocity, ForceMode2D.Impulse);
+        //playerVelocity = GameObject.Find(playerName).GetComponent<Rigidbody2D>().velocity;
+        //rb.AddForce(playerVelocity, ForceMode2D.Impulse);
         rb.AddForce(transform.up * fireForce, ForceMode2D.Impulse);
         Invoke("Despawn", lifeTime);
     }
